f9986a87c870e0596883e1951ccd3ada3b8dc041
José Ferreira <joseguedes.dev@gmail.com>
2023-04-27 21:22:19 -0300

    Starting the Application Backend
    Starting the back-end of the app with structuring the SQLite database
    and the routes. In addition to backend integration with the database
    using sequelize, body-parser and sqlite3 packages.

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const db = require('./db/connection');
+const bodyParser = require('body-parser');
+
+
+const PORT = 3000;
+
+app.listen(PORT, function() {
+	console.log(`O express está rodando na porta ${PORT}`);
+});
+
+// body parser
+app.use(bodyParser.urlencoded({ extented: false }));
+
+// db connection
+db
+	.authenticate()
+	.then(() => {
+		console.log("Connected to database successfully");
+	})
+	.catch(err => {
+		console.log("Connected to database successfully", err);
+	});
+
+// routes
+app.get('/', (req, res) => {
+	res.send("Está funcionando 25");
+});
+
+// job routes
+app.use('/jobs', require('./routes/jobs'));
